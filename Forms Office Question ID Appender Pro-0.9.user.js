d617b12564bf55c8c4dcbf0a1e13a28112ef2d70
dvlart <dvlbross@gmail.com>
2024-07-08 14:24:04 +0100

    Update Forms Office Question ID Appender Pro-0.9.user.js

--- Forms Office Question ID Appender Pro-0.9.user.js
+++ Forms Office Question ID Appender Pro-0.9.user.js
@@ -4,6 +4,8 @@
 // @version      0.9.1
 // @description  Appends question IDs to question text in forms.office.com with format options, inline copy buttons, and custom format generation. Features draggable and minimizable modal.
 // @match        https://forms.office.com/*
+// @downloadURL    https://github.com/dvlart/MSForms-ID/raw/main/Forms%20Office%20Question%20ID%20Appender%20Pro-0.9.user.js
+// @updateURL    https://github.com/dvlart/MSForms-ID/raw/main/Forms%20Office%20Question%20ID%20Appender%20Pro-0.9.user.js
 // @grant        none
 // ==/UserScript==
 
